6c83bb3ae0576ca816c2e6353885bb08fb3d6e25
KashaGef113 <98354683+KashaGef113@users.noreply.github.com>
2023-04-28 18:00:10 +0300

    Update main.py
    version 1.1

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ class Question():
     def __init__(self, question_text, right_answer, wrong1, wrong2, wrong3):
         self.q_text = question_text
         self.r_answer = right_answer
-        
+        self.w1 = wrong1
+        self.w2 = wrong2
+        self.w3 = wrong3
+
+questions_list = []
+q1 = Question('Самая быстрая машина в мире?', 'Koenigsegg Jesko Absolut', 'Lamborghini Huracan', 'Bugatti Chiron', 'Ferrari')
+q2 = Question('Самая дорогая машина в мире?', 'Ferrari 335 S Spider', 'Rolce Royse Fantom', 'Bugatti Veyron', 'Lamborghini Aventador')
+q3 = Question('Самый мощный ноутбук в мире?', 'GPD Win Max 2', 'ASUS TUF', 'ASUS Rog', 'HP')
+questions_list.append(q1)
+questions_list.append(q2)
